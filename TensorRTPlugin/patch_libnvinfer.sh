0793b482398e844d4aa0d9cd0f2f71de8d72f5a9
Levi Pereira <levi.pereira@gmail.com>
2024-05-23 19:14:06 -0300

    Updated README and fix somes bugs

--- TensorRTPlugin/patch_libnvinfer.sh
+++ TensorRTPlugin/patch_libnvinfer.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
+
 arch=$(uname -m)
 
 if [ "$arch" == "x86_64" ]; then
     TRT_INSTALL_LIBPATH=/usr/lib/$arch-linux-gnu
 else
     echo "$arch not supported yet."
-    exit
-fi
-
-# Check if libnvinfer_plugin.so.8.6.1 exists in the current directory
-if [ ! -f "./libnvinfer_plugin.so.8.6.1" ]; then
-    echo "Error: Build or download libnvinfer_plugin.so.8.6.1"
     exit 1
 fi
 
-# Backup original libnvinfer_plugin.so.x.y
+download_library() {
+    local url="https://github.com/levipereira/triton-server-yolo/releases/download/v0.0.1/libnvinfer_plugin.so.8.6.1"
+    echo "Downloading libnvinfer_plugin.so.8.6.1 from $url"
+    curl -L -o libnvinfer_plugin.so.8.6.1 "$url"
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to download libnvinfer_plugin.so.8.6.1"
+        exit 1
+    fi
+}
+
+if [ "$1" == "--download" ]; then
+    download_library
+fi
+
+if [ ! -f "./libnvinfer_plugin.so.8.6.1" ]; then
+    echo "Error: libnvinfer_plugin.so.8.6.1 not found. Build or download it with flag --download"
+    exit 1
+fi
+
 if [ -f "$TRT_INSTALL_LIBPATH/libnvinfer_plugin.so.8.6.1" ]; then
     cp $TRT_INSTALL_LIBPATH/libnvinfer_plugin.so.8.6.1 "./libnvinfer_plugin.so.8.6.1.ori.bak"
+    echo "Original libnvinfer_plugin.so.8.6.1 was copied to ./libnvinfer_plugin.so.8.6.1.ori.bak"
 fi
 
 cp $(pwd)/libnvinfer_plugin.so.8.6.1 $TRT_INSTALL_LIBPATH/libnvinfer_plugin.so.8.6.1
 
-# Update library cache
 ldconfig
 echo "libnvinfer_plugin.so.8.6.1 has been installed successfully."
-echo "Original libnvinfer_plugin.so.8.6.1 was copied to ./libnvinfer_plugin.so.8.6.1.ori.bak"
